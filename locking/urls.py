3691617ae43026e998f31141ca39817d4f7753e6
Jeremy Satterfield <jsatt22@gmail.com>
2013-12-11 13:57:36 -0600

    handle deprecation warnings when using django 1.5

--- locking/urls.py
+++ locking/urls.py
@@ -1,13 +1,20 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    from django.conf.urls.defaults import patterns
 
-urlpatterns = patterns('locking.views',
+urlpatterns = patterns(
+    'locking.views',
     # verwijst naar een ajax-view voor het lockingmechanisme
     (r'(?P<app>[\w-]+)/(?P<model>[\w-]+)/(?P<id>\d+)/lock/$', 'lock'),
     (r'(?P<app>[\w-]+)/(?P<model>[\w-]+)/(?P<id>\d+)/unlock/$', 'unlock'),
-    (r'(?P<app>[\w-]+)/(?P<model>[\w-]+)/(?P<id>\d+)/is_locked/$', 'is_locked'),
+    (r'(?P<app>[\w-]+)/(?P<model>[\w-]+)/(?P<id>\d+)/is_locked/$',
+     'is_locked'),
     (r'variables\.js$', 'js_variables', {}, 'locking_variables'),
-    )
+)
 
-urlpatterns += patterns('',
-    (r'jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': 'locking'}),
-    )
+urlpatterns += patterns(
+    '',
+    (r'jsi18n/$', 'django.views.i18n.javascript_catalog',
+     {'packages': 'locking'}),
+)
